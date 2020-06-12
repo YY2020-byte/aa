df4c7fbace2d86cf609834ae9958a660992f0433
Lenovo <Lenovo@LAPTOP-FP4RF41K>
2020-06-12 09:13:10 +0800

    添加项目文件。

---
+++ aa/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
